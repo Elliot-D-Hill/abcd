0f4bcb8f22907e7e8ca2c36f70742af731c65189
Elliot-D-Hill <elliot.douglas.hill@gmail.com>
2024-12-28 00:52:03 -0500

    Update trainer configuration to set devices to 1 and limit validation batches to 0

--- src/abcd/model.py
+++ src/abcd/model.py
@@ -246,12 +246,12 @@ def make_trainer(cfg: Config, checkpoint: bool) -> Trainer:
         logger=logger,
         callbacks=callbacks,
         accelerator="auto",
-        devices=-1,
-        num_nodes=num_nodes,  # TODO get from environment
+        devices=1,
+        num_nodes=num_nodes,
         strategy="auto",
         log_every_n_steps=cfg.logging.log_every_n_steps,
         fast_dev_run=cfg.fast_dev_run,
-        check_val_every_n_epoch=1,
+        limit_val_batches=0,
         enable_checkpointing=checkpoint,
         **cfg.trainer.model_dump(),
     )
